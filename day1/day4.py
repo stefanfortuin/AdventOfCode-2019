5472880ee851f7623e43dcdd2ca29b4dfdabfa4c
Stefan Fortuin <stefanfortuin02@gmail.com>
2019-12-09 20:54:44 +0100

    day 4 done

--- day1/day4.py
+++ day1/day4.py
@@ -1,57 +1,40 @@
 range_start = 248345
 range_end = 746315
+from re import finditer
 
 def hasDouble(password):
-	for i in range(0,6):
-		if (i+1 > 5):
-			return False
-
+	for i in range(0,5):
 		if(int(password[i]) == int(password[i+1])):
 			return True
 
 def hasMultipleDouble(password):
-	doubles = []
-	for i in range(0,6):
-		if (i+1 > 5):
-			break
-
-		if(int(password[i]) == int(password[i+1])):
-			if (len(doubles) > 0 and doubles[int(password[i])] == int(password[i])):
-				doubles[int(password[i])][1] += 1
-			else:
-				doubles.append([int(password[i]), 1])
-	
-	print(doubles)
-	test = all(doubles[i][1] % 2 != 0 for i in doubles)
-	print(test)
-
-	return False
-
-def isIncreasing(password):
-	for n in range(0,6):
-		if ( n == 5):
+	for match in finditer(r'(\d)\1+', password):
+		if len(match.group(0)) == 2:
 			return True
 
+	# from internet, interpreted the criteria wrong
+	# return any(len(match.group(0)) == 2 for match in finditer(r'(\d)\1+', password))
+	
+def isIncreasing(password):
+	for n in range(0,5):
 		if (int(password[n]) > int(password[n+1])):
 			return False
-	
+
 	return True
 
-password = 111122
-if (hasMultipleDouble(str(password)) and isIncreasing(str(password))):
-	print("yes")
-else:
-	print("no")
-# total = 0
-# for password in range(range_start, range_end):
-# 	if (hasDouble(str(password)) and isIncreasing(str(password))):
-# 		total += 1
 
-# print(f"result 1: {total}")
+total = 0
+first_pass = []
+for password in range(range_start, range_end):
+	if (hasDouble(str(password)) and isIncreasing(str(password))):
+		first_pass.append(password)
+		total += 1
 
-# total2 = 0
-# for password in range(range_start, range_end):
-# 	if (hasMultipleDouble(str(password)) and isIncreasing(str(password))):
-# 		total2 += 1
+print(f"result 1: {total}")
 
-# print(f"result 2: {total2}")
+total2 = 0
+for password in first_pass:
+	if hasMultipleDouble(str(password)):
+		total2 += 1
+
+print(f"result 2: {total2}")
